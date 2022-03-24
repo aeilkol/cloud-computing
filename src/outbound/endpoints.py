3fc73264db95769e30b6f9590177c8c59ce95e9d
Aeilko Lübsen <aeilko_luebsen@web.de>
2022-03-24 17:27:59 +0000

    initialized outbound api with flask and connexion

---
+++ src/outbound/endpoints.py
@@ -0,0 +1,12 @@
+
+def read_all_airports():
+    pass
+
+def read_all_flights():
+    pass
+
+def read_all_covid_cases():
+    pass
+
+def read_airport_covid_cases():
+    pass
---
+++ src/outbound/outbound.py
@@ -0,0 +1,7 @@
+import connexion
+
+app = connexion.FlaskApp(__name__, specification_dir='../../openapi')
+app.add_api('flightsAndCovid.yml')
+
+if __name__ == '__main__':
+    app.run(port=8080, debug=True)
