79224d98be806c2a56567f3c8abb1a0bf161e53f
Aeilko Lübsen <aeilko_luebsen@web.de>
2022-03-29 13:01:59 +0100

    fixed airport endpoint

--- src/microservices/data_delivery/data_delivery.py
+++ src/microservices/data_delivery/data_delivery.py
@@ -3,13 +3,16 @@ import os
 from concurrent import futures
 
 import dotenv
-import psycopg2
+import psycopg2, psycopg2.extras
 import grpc
 from grpc_interceptor import ExceptionToStatusInterceptor
+import shapely, shapely.wkb
 
 from data_delivery_pb2 import (
     Airport,
-    AirportResponse
+    AirportResponse,
+    Location,
+    AirportType
 )
 import data_delivery_pb2_grpc
 
@@ -20,13 +23,17 @@ class DataDeliveryService(data_delivery_pb2_grpc.DataDeliveryServicer):
         self.database_service = DataDeliveryDatabaseService(connection)
 
     def Airports(self, request, context):
-        airports = self.database_service.getAirports(request.continent)
+        airport_type = AirportType.Name(request.airport_type)
+        airports = self.database_service.getAirports(request.continent, airport_type)
         airport_objects = []
         for airport in airports:
+            shapely_location = shapely.wkb.loads(airport['location'], hex=True)
             airport_objects.append(
                 Airport(
                     name=airport['name'],
-                    code=airport['code']
+                    code=airport['code'],
+                    location=Location(latitude=shapely_location.y, longitude=shapely_location.x),
+                    type=airport['type']
                 )
             )
         return AirportResponse(airports=airport_objects)
@@ -37,14 +44,18 @@ class DataDeliveryDatabaseService():
     def __init__(self, connection):
         self.connection = connection
 
-    def getAirports(self, continent=None):
-        sql = 'SELECT * FROM airports'
-        params = []
-        if continent:
-            sql += ' WHERE continent=%s'
-            params.append(continent)
-        cursor = self.connection.cursor()
-        cursor.query(sql, params)
+    def getAirports(self, continent=None, airport_type=None):
+        sql = '''
+            SELECT * FROM airports
+            WHERE (continent = %(continent)s OR %(continent)s IS NULL)
+              AND ("type" = %(airport_type)s OR %(airport_type)s IS NULL)
+            '''
+        params = {
+            'continent': continent,
+            'airport_type': airport_type
+        }
+        cursor = self.connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        cursor.execute(sql, params)
         return cursor.fetchall()
 
 
--- src/outbound/endpoints.py
+++ src/outbound/endpoints.py
@@ -1,23 +1,29 @@
 import grpc
+from google.protobuf.json_format import MessageToJson
 
 from data_delivery_pb2 import Airport, AirportRequest
 from data_delivery_pb2_grpc import DataDeliveryStub
 
-data_delivery_channel = grpc.insecure_channel('localhost:50051')
+data_delivery_channel = grpc.insecure_channel('localhost:50051', options=(('grpc.enable_http_proxy', 0),))
 data_delivery_client = DataDeliveryStub(data_delivery_channel)
+
 
 def read_all_airports(continent, airport_type):
     request = AirportRequest(
-        continent=continent
+        continent=continent,
+        airport_type=airport_type
     )
     response = data_delivery_client.Airports(request)
-    return response
+    return MessageToJson(response)
+
 
 def read_all_flights():
     pass
 
+
 def read_all_covid_cases():
     pass
+
 
 def read_airport_covid_cases():
     pass
